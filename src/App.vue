4f537201a9e479bf8b4aa91e7b0bb2b54f69a4bf
zhoujing01 <zhoujing01@jsfund.cn>
2024-07-10 14:30:16 +0800

    feat: 导航栏

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,44 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView, useRoute } from 'vue-router'
+
+import { ElConfigProvider } from 'element-plus'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import { computed, ref } from 'vue';
+import Logo from './components/Logo.vue';
+
+const locale = ref(zhCn);
+const route = useRoute();
+const showNav = computed(() => route.name !== 'login');
+
 </script>
 
 <template>
-  <RouterView />
+  <ElConfigProvider :locale="locale">
+    <ElContainer style="height: 100%;">
+      <ElHeader v-if="showNav">
+        <div class="row logo-container">
+          <Logo />
+        </div>
+      </ElHeader>
+      <ElMain>
+        <RouterView />
+      </ElMain>
+    </ElContainer>
+  </ElConfigProvider>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<style lang="scss" scoped>
+.logo-container {
+  align-items: center;
+  height: 100%;
+  padding: 0 24px;
+}
+</style>
+
+<style>
+.el-header {
+  --el-header-padding: 0;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, .06);
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
